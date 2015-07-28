6c6073923e6f2028f3176b1808ef6b71bdcec8b2
nalantianyi <103520146@qq.com>
2015-07-28 17:02:24 +0800

    初始化

--- app.js
+++ app.js
@@ -2,5 +2,10 @@
  * Created by nalantianyi on 15/7/28.
  */
 
-
+var express=require('express'),
+    app=express();
+app.get('/',function(req,res,next){
+    res.send('正在开发,尽请期待');
+});
+app.listen(1314);
 
